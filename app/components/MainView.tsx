d7f0ace68485810189cd39ebfaf5c8c4bb7402b2
Adam <adamhinckley@mac.com>
2026-01-26 18:23:54 -0600

    Add email auth

--- app/components/MainView.tsx
+++ app/components/MainView.tsx
@@ -9,7 +9,6 @@ const MainView = () => {
 
   return (
     <main className="min-h-screen  p-4 max-w-6xl mx-auto">
-      <h1 className="text-2xl   mb-4">Stock Watcher</h1>
       <StockIInput />
       {watchingStocks.length > 0 && (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mt-6">
