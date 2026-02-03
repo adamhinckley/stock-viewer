e2406d1508544abb9563b92611f417bdbb42bc05
Adam <adamhinckley@mac.com>
2026-02-03 10:06:30 -0600

    Remocve unndecessary env config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  env: {
-    API_KEY: process.env.API_KEY,
-    API_URL: process.env.API_URL,
-  },
+  // env: {
+  //   API_KEY: process.env.API_KEY,
+  //   API_URL: process.env.API_URL,
+  // },
   images: {
     remotePatterns: [
       {
